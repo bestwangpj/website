f3b7990272a782633982816316d73fbf08803214
wangpengjian <wangpengjian@zhijiabest.com>
2019-09-24 18:17:54 +0800

    update code

---
+++ build/precache-manifest.12e67517818f92eba9ca4782051d678c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d682eb2216303eb581c7700b71334b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53dcd4175a12f13453a0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d5719d780173e27db77f",
+    "url": "/static/js/2.8567c72f.chunk.js"
+  },
+  {
+    "revision": "53dcd4175a12f13453a0",
+    "url": "/static/js/main.1355ab2d.chunk.js"
+  },
+  {
+    "revision": "a659c513b913be4e7b37",
+    "url": "/static/js/runtime-main.d52357dd.js"
+  }
+]);
